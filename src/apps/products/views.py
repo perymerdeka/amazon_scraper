722ef60cf81de4eef23fd345540e3d966d0b6312
perymerdeka <ferilukmansyah@email.com>
2023-11-13 17:41:17 +0700

    WIP: Define Scraper Route

--- src/apps/products/views.py
+++ src/apps/products/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from loguru import logger
+
+from services.scraper.runner import Runner
+from apps.products.serializers import ProductModelSerializer
+from apps.products.models import ProductsModel
+
 
 # Create your views here.
+
+
+class ScrapeView(APIView):
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        runner: Runner = Runner()
+        try:
+            keyword = request.data.get("keyword", "")
+            page_number = request.data.get("page_number", "1")
+
+            product_data = runner.search_product(
+                keyword=keyword, page_number=page_number
+            )
+            for product in product_data:
+                existed_data = ProductsModel.objects.filter(
+                    product_link=product["product_link"]
+                ).first()
+                if existed_data:
+                    logger.info("Data Is Existed return data from database")
+                    serializer = ProductModelSerializer(data=existed_data)
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+                else:
+                    logger.info("Data Is Not In Database Saved Scraping data")
+                    serializer = ProductModelSerializer(data=product)
+                    if serializer.is_valid():
+                        logger.info("Saved to database")
+                        serializer.save()
+                        return Response(serializer.data, status=status.HTTP_201_CREATED)
+                    else:
+                        return Response(
+                            {"error": "Invalid data"},
+                            status=status.HTTP_400_BAD_REQUEST,
+                        )
+
+        except Exception as e:
+            return Response(
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
--- src/apps/products/viewsets.py
+++ src/apps/products/viewsets.py
@@ -16,21 +16,5 @@ class ProductModelViewSet(ModelViewSet):
         serializer = self.get_serializer(products, many=True)
         return Response(serializer.data)
 
-    # running the scraper method
-    def create(self, request, *args, **kwargs):
 
-        # define keyword
-        query: str = request.data.get("query")
-        page_number: str = request.data.get("page", "1")
 
-        # define scraper
-        scraper: Runner = Runner()
-        product_data: list[dict[str, Any]] = scraper.search_product(keyword=query, page_number=page_number)
-        
-        serializer = self.get_serializer(data=product_data)
-
-        # data validation
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return super().create(request, *args, **kwargs)
